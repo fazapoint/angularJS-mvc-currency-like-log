fc0b7449b3d961f6812ef2c1a32c7fa7affa2312
fazapoint <fazaramadhani99@gmail.com>
2020-04-15 13:41:56 +0700

    First

---
+++ js/controller/MainController.js
@@ -0,0 +1,32 @@
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.title = 'Belajar AngularJs';
+    $scope.books = [
+        {
+            title: 'Belajar AngularJs Bersama Agung Setiawan', author: 'Agung Setiawan',
+            price: 80000,
+            pubdate: new Date('2015', '09', '09'),
+            likes: 0
+        },
+        {
+            title: 'Belajar Ruby', author: 'Penulis Satu', price: 65000,
+            pubdate: new Date('2015', '10', '09'),
+            likes: 0
+        },
+        {
+            title: 'Belajar Ruby on Rails', author: 'Penulis Dua',
+            price: 90000,
+            pubdate: new Date('2015', '11', '09'),
+            likes: 0
+        }];
+
+    $scope.logToConsole = function (index) {
+        var book = $scope.books[index];
+        console.log(book);
+    };
+
+    $scope.likes = function (index) {
+        $scope.books[index].likes += 1;
+    };
+
+
+}]);
